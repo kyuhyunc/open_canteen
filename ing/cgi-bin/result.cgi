263bfa59a5d1c4b92ae3654d69938cbdc7190ed1
Kyu Chang <kyuhyunc@usc.edu>
2014-04-21 22:23:21 +0800

    implemented the function to check if input has made approprieately

--- ing/cgi-bin/result.cgi
+++ ing/cgi-bin/result.cgi
@@ -4,15 +4,11 @@ use strict;
 use CGI qw(:standard);
 use CGI::Carp qw(warningsToBrowser fatalsToBrowser);
 
-#use HTTP::Request::Common qw(POST);
-#use LWP::UserAgent;
-
-#use HTTP::Request::Common;
-#my $cgi = new CGI;
-#$cgi->header(-type=>"text/html");
-
 my @food_type = param('food_type');
 my $canteen = param('canteen');
+my $food_name = param('food_name');
+
+# Check if inputs have been selected/entered properly
 
 print header,
     start_html('Open Canteen:result'),
@@ -21,6 +17,8 @@ print header,
     "Selected Food Types: @food_type",
     p(),
     "Selected Canteen: $canteen",
+    p(),
+    "Name of Food: $food_name",
     p(),
     a({-href=>'/~kchangaa'}, "Go back to the main page."),
     end_html;
---
+++ ing/open_canteen.js
@@ -0,0 +1,27 @@
+// The load function for the SVG document
+function load(evt) {
+    // Set the root node to the global variable
+    svgdoc = evt.target.ownerDocument;
+}
+
+// Check if inputs have been selected/entered properly
+function validateForm()
+{
+    var food_type = svgdoc.getElementsByName("food_type");
+    var canteen = svgdoc.getElementsByName("canteen");
+    var err_flag1 = true;
+    var err_flag2 = true;
+    
+    for (var i=0; i<food_type.length; i++) {
+        if(food_type[i].checked == true) err_flag1 = false;
+    }
+
+    for (var i=0; i<canteen.length; i++) {
+        if(canteen[i].checked == true) err_flag2 = false;
+    }
+
+    if (err_flag1 || err_flag2) {
+        alert("Food type and Canteen must be selected");
+        return false;
+    }
+}
